9d690960b8fb2f36e3fec618083a5eeb0e322725
tosinplus99 <tosinplus99@gmail.com>
2023-07-17 07:04:24 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
-
 /**
- * main - Prints numbers between 00 to 89.
- * 
- * Return: Always 0 (Success)
- */
+*main - Prints all combinations of two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
-	int i, e;
+int digit1, digit2;
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1 ; digit2 < 10; digit2++)
+{
 
-	i = 48;
-	e = 48;
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
-	while (e < 58)
-	{
-		i = 48;
-		while (i < 58)
-		{
-			if (e != i && e < i)
-			{
-				putchar(e);
-				putchar(i);
-				if (i == 57 && e == 56)
-				{
-					break;
-				}
-				putchar(',');
-				putchar(' ');
-			}
-			i++;
-		}
-		e++;
-	}
-	putchar('\n');
-	return (0);
+if (digit1 == 8 && digit2 == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+
+}
+
+
+}
+putchar('\n');
+return (0);
 }
